60f2ed79809f00ad1c14e3a01d780f6dd7a018af
sude <sudefidan@icloud.com>
2024-10-13 17:06:30 +0300

    while-ornek

---
+++ 20232907_While_Ornek3/20232907_While_Ornek3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20232907_While_Ornek3
+{
+    internal class Program
+    {
+        //Kullanıcıdan 2 adet sayı talep edelim
+        //1.sayıdan 2.sayıya kadar olan sayıların toplamını ve ortalamasını ekrana yazdıralım
+        static void Main(string[] args)
+        {  
+            int ilkSayi, ikinciSayi, toplam = 0, sayac = 0;
+            double ortalama;
+            Console.Write("1.Sayıyı Giriniz : ");
+            ilkSayi=int.Parse(Console.ReadLine());
+            Console.Write("2.Sayıyı Giriniz : ");
+            ikinciSayi=int.Parse(Console.ReadLine());
+            if (ilkSayi==ikinciSayi)
+            {
+                Console.WriteLine("Sayılar Birbirine Eşittir ");
+            }
+            else
+
+            {
+                //Birinci Yöntem    
+
+                /*
+                if (ilkSayi>ikinciSayi)
+                {       
+                    int gecici = ilkSayi;
+                    ilkSayi = ikinciSayi;
+                    ikinciSayi= gecici;
+                }      
+
+                while (ilkSayi<=ikinciSayi) 
+                {
+                    toplam += ilkSayi; // toplam = toplam +ilkSayi;
+                    ilkSayi++;
+                    sayac++;
+
+                } 
+                */
+                
+                //İkinci Yöntem
+                if (ilkSayi>ikinciSayi)
+                {
+                    while (ilkSayi>=ikinciSayi)
+                    {
+                        toplam += ilkSayi;
+                        ilkSayi--;
+                        sayac++;
+
+                    }
+                }
+                else
+                {
+                    while (ilkSayi<=ikinciSayi)
+                    {
+                        toplam += ilkSayi;
+                        ilkSayi++;
+                        sayac++;
+
+                    }
+                }
+                Console.WriteLine("Sayıların Toplamı = " +toplam);
+                ortalama=(double)toplam/sayac;
+                Console.WriteLine("Sayıların Ortalaması = "+ortalama);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
